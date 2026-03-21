d1399afda78d1e266a162a6f92308ded28b3e2a7
omartaoufiqui9-droid <omartaoufiqui9@gmail.com>
2026-03-21 18:25:49 +0000

    Update jabbar.py

--- jabbar.py
+++ jabbar.py
@@ -100,4 +100,22 @@ else:
 
     with tab3:
         st.header("🐥 أفلام كرتون")
-        st.video("https://www.youtube.com/watch?v=tC_69fLwI-Y")
+        st.video("https://www.youtube.com/watch?v=tC_69fLwI-Y") 
+# سطر جديد من الأفلام
+        col4, col5, col6 = st.columns(3)
+        with col4:
+            st.markdown("<div class='movie-card'>", unsafe_allow_html=True)
+            st.subheader("فيلم جديد 1")
+            st.video("ضع_رابط_اليوتيوب_هنا")
+            st.markdown("</div>", unsafe_allow_html=True)
+        with col5:
+            st.markdown("<div class='movie-card'>", unsafe_allow_html=True)
+            st.subheader("فيلم جديد 2")
+            st.video("ضع_رابط_اليوتيوب_هنا")
+            st.markdown("</div>", unsafe_allow_html=True)
+        with col6:
+            st.markdown("<div class='movie-card'>", unsafe_allow_html=True)
+            st.subheader("فيلم جديد 3")
+            st.video("ضع_رابط_اليوتيوب_هنا")
+            st.markdown("</div>", unsafe_allow_html=True)
+            
